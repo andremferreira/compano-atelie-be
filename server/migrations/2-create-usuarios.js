e74ebd2c221009228f27ac08e73d68bcc2e8f23c
André <ferreiraandremendes@gmail.com>
2019-10-02 17:17:49 -0300

    Configuracao inicial
    Modelando estrutura de banco de dados.

---
+++ server/migrations/2-create-usuarios.js
@@ -0,0 +1,79 @@
+'use strict';
+const withDateNoTz = require('sequelize-date-no-tz-postgres');
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    const sequelize = withDateNoTz(Sequelize)
+    return queryInterface.createTable({
+      schema: 'atelie',
+      schemaDelimiter: '.',
+      modelName: 'atelie',
+      tableName: 'usuarios'
+    }, {
+      id_usuario: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      vc_nome: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+        validate: {
+          len: [3, 100],
+          msg: "Only allow values with length between 3 and 50 characters."
+        },
+      },
+      vc_sobrenome: {
+        allowNull: false,
+        type: Sequelize.STRING(100),
+        validate: {
+          len: [5, 100],
+          msg: "Only allow values with length between 5 and 100 characters."
+        },
+      },
+      vc_email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(100),
+        validate: {
+          len: [5, 100],
+          msg: "Only allow values with length between 5 and 100 characters."
+        },
+      },
+      vc_senha: {
+        allowNull: false,
+        type: Sequelize.STRING(32)
+      },
+      tx_imagem: {
+        type: Sequelize.TEXT
+      },
+      vc_reset_senha: {
+        type: Sequelize.STRING(32)
+      },
+      dt_exp_reset: {
+        type: sequelize.DATE_NO_TZ
+      },
+      dt_cadastro: {
+        allowNull: false,
+        type: sequelize.DATE_NO_TZ,
+        defaultValue: Sequelize.fn('now'),
+      },
+      dt_atualiacao: {
+        allowNull: false,
+        type: sequelize.DATE_NO_TZ,
+        defaultValue: Sequelize.fn('now'),
+      }
+    }, {});
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable({
+      schema: 'atelie',
+      schemaDelimiter: '.',
+      modelName: 'atelie',
+      tableName: 'usuarios'
+    }, {
+      force: true,
+      cascade: true
+    });
+  }
+};
---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+//const userController = require('../controllers').User;
+module.exports = (app) => {
+//teste do serviço
+app.get('/api', (req, res) => res.status(200).send({message: 'Entrou em rotas dentro da api!'}));
+//
+// CRUD tabela user
+//app.get('/api/user', groupController.list);
+//app.get('/api/user/:id', groupController.getById);
+//app.post('/api/user/', groupController.add);
+//app.put('/api/user/:id', groupController.update);
+//app.delete('/api/user/:id', groupController.delete);
+// Set sequência do id json_obj
+//app.post('/api/seqgrupo/:id', groupController.setNextSeq);
+
+};
